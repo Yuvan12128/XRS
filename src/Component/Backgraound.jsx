e72984881813323261cb5a4e02f5b166b73ae530
Yuvan12128 <yuvan12128@.com>
2025-02-05 11:33:45 +0530

    update

--- src/Component/Backgraound.jsx
+++ src/Component/Backgraound.jsx
@@ -13,7 +13,7 @@ const Backgraound = () => {
           <div className="container mx-auto p-5">
           <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-              Teal to Lime
+             Book Now !
             </span>
           </button>
           </div>
--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -8,7 +8,7 @@ import p5 from './p5.jpeg'
 const Portfolio = () => {
   return (
 <>
-<div className="container mx-auto">
+<div className="container mx-auto bg-gray-200 p-10 rounded-lg">
        <div className="container">
        <h1 class="mt-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black text-center p-4">
         
